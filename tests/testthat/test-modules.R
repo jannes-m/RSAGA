d520b8bfc73f4ab28ab64b4e9b6147a138de1815
Marc Becker <marcbecker@posteo.de>
2017-12-20 21:55:56 +0100

    Minimal testthat

--- tests/testthat/test-modules.R
+++ tests/testthat/test-modules.R
@@ -8,6 +8,7 @@ test_that("Write DEM to disc", {
   env <- rsaga.env2()
 
   data(landslides)
+  
   write.sgrd(data = dem, file = "dem", header = dem$header, env = env, 
              check.module.exists=FALSE)
   
@@ -15,20 +16,6 @@ test_that("Write DEM to disc", {
   
   expect_true(digest(test, algo="md5") %in% c("137181ee7294515ceb6ccf04fe975cfd"))
   
-})
-
-test_that("Slope", {
-  
-  env <- rsaga.env2()
-  
-  rsaga.slope.asp.curv("dem", out.slope = "slope", out.cprof = "cprof", 
-                       out.cplan = "cplan", method = "poly2zevenbergen",
-                       env = env, check.module.exists=FALSE) 
-  
-  test <- read.sgrd("slope.sgrd", env=env, check.module.exists=FALSE)
-  
-  expect_true(digest(test, algo="md5") %in% c("b2852c8fa289636908e1322ee33c3c0b",
-                                              "b1b0c8c02db274cad530d2021abd7032"))
 })
 
 
